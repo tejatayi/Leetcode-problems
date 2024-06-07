54a4e0d5a1582bed1480cc6d79454a594adad384
tejatayi <saisivatejatayi@gmail.com>
2024-06-07 15:38:30 -0500

    Missing Number

---
+++ problem268.java
@@ -0,0 +1,34 @@
+// 268. Missing Number
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Problem268{
+
+  
+        public static int missingNumber(int[] n) {
+    
+           int x= n.length;
+            int total= (x*(x+1))/2;
+        int sum=0;
+        for (int i=0;i<x;i++)
+        sum+=n[i]; 
+    
+        return total-sum;
+    
+        }
+
+
+
+
+
+    public static void main(String[] args)
+    {
+        
+        int []n={0,1,2,3,4,5,6,7,9};
+
+       System.out.println(missingNumber(n));
+       
+        return;
+    }
+}
