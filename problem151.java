32de24d366476daca71389cb8776d838902e6ac6
tejatayi <saisivatejatayi@gmail.com>
2024-06-19 21:01:14 -0500

    Reverse Words in a String

---
+++ problem151.java
@@ -0,0 +1,44 @@
+//151. Reverse Words in a String
+
+import java.util.*;
+
+class Problem151 {
+
+    public static String reverseWords(String s) {
+        
+       String [] x = s.split(" ");
+       
+       String ans = "";
+       for(int i=(x.length)-1;i>=0;i--)
+       {
+            if(x[i]!="")
+            {
+                ans+=x[i];
+                ans+=" ";
+            }
+        
+       }
+      
+    String fin="";
+    
+    for(int i=0;i<ans.length()-1;i++)
+    {
+        fin+=ans.charAt(i);
+    }
+    
+//      System.out.println('"'+fin+'"');       
+
+
+        return fin;
+    }
+
+    public static void main(String[] args){
+       
+            String x = "the sky is blue";
+
+            reverseWords(x);
+
+           // System.out.println();
+        return ;
+    }
+}
