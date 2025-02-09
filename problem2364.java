4ea4a3b078e62b08d4c2e8f4df5135469c49a643
tejatayi <saisivatejatayi@gmail.com>
2025-02-09 12:52:06 -0500

    Count Number of Bad Pairs

---
+++ problem2364.java
@@ -0,0 +1,51 @@
+//2364. Count Number of Bad Pairs
+
+import java.util.HashMap;
+
+public class problem2364 {
+    
+
+
+
+    public static long countBadPairs(int[] nums) {
+        
+        long len = nums.length;
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i =0;i<len;i++)
+        {
+            
+            int tem = nums[i] - i;
+            map.merge(tem, 1, Integer::sum);
+    
+        }
+        long ans = ((len*(len-1))/2);
+        
+
+        for(int m : map.values())
+        {
+            ans-=(m*(m-1))/2;
+        }
+
+       
+
+
+        return ans;
+
+    }
+
+
+   
+
+
+
+    public static void main(String[] args) {
+        
+        int [] a = {4,1,3,3};
+
+        System.out.println(countBadPairs(a));
+
+
+
+    }
+}
